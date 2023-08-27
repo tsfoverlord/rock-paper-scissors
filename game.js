2ddb15acab0a486af3fb69a38a5b4acd0c64d2eb
tsfoverlord <rat4521@gmail.com>
2023-08-27 23:35:31 +0530

    Add playRound function which returns who wins

--- game.js
+++ game.js
@@ -3,3 +3,22 @@ function  getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length);
     return choices[choice];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    if(playerSelection == "rock" && computerSelection == "paper"){
+        return "You Lose! paper beats rock";
+    }
+    else if(playerSelection == "paper" && computerSelection == "scissor") {
+        return "You lose! scissor beats paper";
+    }
+    else if(playerSelection == "scissor"  && computerSelection == "rock") {
+        return "You lose! rock beats scissor"
+    }
+    else {
+        return `You win ${playerSelection} beats ${computerSelection}`
+    }
+}
